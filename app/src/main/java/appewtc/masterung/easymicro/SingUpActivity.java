5461f00f8fb85b4fbef62755842bd27f50e4f0ec
masterUNG <phrombutr@gmail.com>
2016-11-05 14:41:08 +0700

    Check Space Finish

--- app/src/main/java/appewtc/masterung/easymicro/SingUpActivity.java
+++ app/src/main/java/appewtc/masterung/easymicro/SingUpActivity.java
@@ -1,13 +1,59 @@
 package appewtc.masterung.easymicro;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SingUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private ImageView imageView;
+    private Button button;
+    private String nameString, userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
-    }
-}
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button3);
+
+        //SignUp Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value From Edit text
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (nameString.equals("") ||
+                        userString.equals("") ||
+                        passwordString.equals("")) {
+
+                    //Have Space
+                    Log.d("5novV1", "Have Space");
+
+                }   // if
+
+            }   // onClick
+        });
+
+
+
+    }   // Main Method
+
+}   // Main Class
