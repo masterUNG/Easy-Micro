aff53b969483b98454f5f036868c7aac1e006ad8
masterUNG <phrombutr@gmail.com>
2016-11-06 16:34:13 +0700

    Finish 6nov

---
+++ app/src/main/java/appewtc/masterung/easymicro/MyAdapter.java
@@ -0,0 +1,59 @@
+package appewtc.masterung.easymicro;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by masterUNG on 11/6/2016 AD.
+ */
+
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context context;
+    private String[] nameStrings, imageStrings;
+
+    public MyAdapter(Context context, String[] nameStrings, String[] imageStrings) {
+        this.context = context;
+        this.nameStrings = nameStrings;
+        this.imageStrings = imageStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return nameStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.my_listview, viewGroup, false);
+
+        TextView textView = (TextView) view1.findViewById(R.id.textView2);
+        ImageView imageView = (ImageView) view1.findViewById(R.id.imageView3);
+
+        textView.setText(nameStrings[i]);
+        Picasso.with(context).load(imageStrings[i]).into(imageView);
+
+
+        return view1;
+    }
+}   // Main Class
--- app/src/main/java/appewtc/masterung/easymicro/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/easymicro/ServiceActivity.java
@@ -2,6 +2,10 @@ package appewtc.masterung.easymicro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class ServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
-    }
-}
+
+        ListView listView = (ListView) findViewById(R.id.livUser);
+
+        final String[] nameStrings = getIntent().getStringArrayExtra("Name");
+        String[] imageStrings = getIntent().getStringArrayExtra("Image");
+
+        MyAdapter myAdapter = new MyAdapter(ServiceActivity.this,
+                nameStrings, imageStrings);
+        listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ServiceActivity.this, "You Click " + nameStrings[i],
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }   // Main Method
+
+}   // Main Class
