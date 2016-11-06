55f40eff3144fe064a7711c68c7cce9be0c82020
masterUNG <phrombutr@gmail.com>
2016-11-06 15:02:13 +0700

    Read JSoN Finish

--- app/src/main/java/appewtc/masterung/easymicro/MainActivity.java
+++ app/src/main/java/appewtc/masterung/easymicro/MainActivity.java
@@ -1,11 +1,18 @@
 package appewtc.masterung.easymicro;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,6 +64,11 @@ public class MainActivity extends AppCompatActivity {
                             getResources().getString(R.string.message_haveSpace));
                     myAlert.myDialog();
                 } else {
+
+                    //No Space
+                    GetUser getUser = new GetUser(MainActivity.this);
+                    getUser.execute(myConstante.getUrlJSoN());
+
                 }
 
 
@@ -65,5 +77,43 @@ public class MainActivity extends AppCompatActivity {
 
 
     }   // Main Method
+
+    private class GetUser extends AsyncTask<String, Void, String> {
+
+        private Context context;
+
+        public GetUser(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(strings[0]).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }   // doIn
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("6novV3", "JSON ==> " + s);
+
+
+        }   // onPost
+
+    }   // Class
+
 
 }   // Main Class นี่คื่อ คลาสหลัก นะจรัา
