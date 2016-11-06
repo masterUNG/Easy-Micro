3c79dba2dd73a9a2cdcd889b979deb9da5017f15
masterUNG <phrombutr@gmail.com>
2016-11-06 14:32:58 +0700

    Check Space OK

--- app/src/main/java/appewtc/masterung/easymicro/MainActivity.java
+++ app/src/main/java/appewtc/masterung/easymicro/MainActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit การประกาศตัวแปร
     private Button signInButton, singUpButton;
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+    private MyConstante myConstante;
 
 
     @Override
@@ -17,9 +21,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myConstante = new MyConstante();
+
         //Bind Widget
         signInButton = (Button) findViewById(R.id.button);
         singUpButton = (Button) findViewById(R.id.button2);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
 
         //SignUp Controller
         singUpButton.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +36,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, SingUpActivity.class));
             }
+        });
+
+
+        //SignIn Controller
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value From Edit Text
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (userString.equals("") || passwordString.equals("")) {
+                    //Have Space
+                    MyAlert myAlert = new MyAlert(MainActivity.this,
+                            R.drawable.kon48,
+                            getResources().getString(R.string.title_haveSpace),
+                            getResources().getString(R.string.message_haveSpace));
+                    myAlert.myDialog();
+                } else {
+                }
+
+
+            }   // onClick
         });
 
 
